712772f005d1d7b5348fac6842d594cb10869a00
lcruan <1416802331@qq.com>
2022-08-09 21:53:53 +0800

    登录代码提交

--- src/api/user.js
+++ src/api/user.js
@@ -3,10 +3,21 @@
  */
 import request from "@/utils/request";
 
-export default const login = data => {
+export const login = data => {
     return request({
         method: 'POST',
         url: '/v1_0/authorizations',
         data
+    })
+}
+
+/*
+  发送验证码
+  注意：每个手机号每分钟1次
+ */
+export const sendSms = mobile => {
+    return request({
+        method: 'GET',
+        url: `/v1_0/sms/codes/${mobile}`
     })
 }
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,81 +1,173 @@
 <template>
-    <div class="login-container">
-        <!-- 导航栏 -->
-        <van-nav-bar class="page-nav-bar" title="登录"/>
+  <div class="login-container">
+    <!-- 导航栏 -->
+    <van-nav-bar class="page-nav-bar" title="登录" />
 
-        <!-- 登录的表单 -->
-        <van-form @submit="onSubmit">
-            <van-field
-                name="手机号"
-                placeholder="请输入手机号"
-                v-model="user.mobile"
-            >
-                <i slot="left-icon" class="toutiao toutiao-shouji"></i>
-            </van-field>
-            <van-field
-                name="验证码"
-                placeholder="请输入验证码"
-                v-model="user.code"
-                >
-                <i slot="left-icon" class="toutiao toutiao-yanzhengma"></i>
-                <template #button>
-                    <van-button class="send-sms-btn" round size="small" type="default">发送验证码</van-button>
-                </template>
-            </van-field>
-            <div class="login-btn-wrap">
-                <van-button class="login-btn" block type="info" native-type="submit">登录</van-button>
-            </div>
-        </van-form>
-    </div>
+    <!-- 登录的表单 -->
+    <!-- 
+            表单验证：
+            1、给 van-field 组件配置 rules 有验证规则
+               参考文档：https://vant-ui.github.io/vant/v2/#/zh-CN/form#rule-shu-ju-jie-gou
+            2、当表单提交的时候会自动触发表单验证
+                如果验证通过，会触发 submit 事件
+                如果验证失败，不会触发 submit
+             -->
+    <van-form ref="loginForm" @submit="onSubmit">
+      <van-field
+        name="mobile"
+        placeholder="请输入手机号"
+        v-model="user.mobile"
+        :rules="userFormRules.mobile"
+        type="number"
+        maxlength="11"
+      >
+        <i slot="left-icon" class="toutiao toutiao-shouji"></i>
+      </van-field>
+      <van-field
+        name="code"
+        placeholder="请输入验证码"
+        v-model="user.code"
+        :rules="userFormRules.code"
+        type="number"
+        maxlength="6"
+      >
+        <i slot="left-icon" class="toutiao toutiao-yanzhengma"></i>
+        <template #button>
+        <!-- time： 倒计时时间 -->
+         <van-count-down v-if="isCountDownShow"
+          :time="1000 * 10" 
+          format="ss s"
+          @finish="isCountDownShow = false" />
+          <van-button 
+           v-else
+           class="send-sms-btn" 
+           round size="small" 
+           type="default"
+           native-type="button"
+           @click="onSendSms"
+            >发送验证码</van-button
+          >
+        </template>
+      </van-field>
+      <div class="login-btn-wrap">
+        <van-button class="login-btn" block type="info" native-type="submit"
+          >登录</van-button
+        >
+      </div>
+    </van-form>
+  </div>
 </template>
 <script>
+import { login, sendSms } from "@/api/user";
 export default {
-        name: 'LoginIndex',
-        data() {
-            return {
-                user: {
-                    mobile: '', // 手机号
-                    code: '' // 验证码
-                }
-            }
-        },
-        created() {
+  name: "LoginIndex",
+  data() {
+    return {
+      user: {
+        mobile: "13911111111", // 手机号
+        code: "246810", // 验证码
+      },
+      userFormRules: {
+        mobile: [
+          {
+            required: true,
+            message: "手机号不能为空",
+          },
+          {
+            pattern: /^1[3|5|7|8|9]\d{9}$/,
+            message: "手机号格式错误",
+          },
+        ],
+        code: [
+          {
+            required: true,
+            message: "请填写验证码",
+          },
+          {
+            pattern: /^\d{6}$/,
+            message: '验证码格式错误'
+          }
+        ],
+      },
+      isCountDownShow: false, // 是否展示倒计时
+    };
+  },
+  created() {},
+  methods: {
+    async onSubmit() {
+      // 1. 获取表单数据
+      const user = this.user;
+      // 2. 表单验证
 
-        },
-        methods: {
-            onSubmit() {
-                // 1. 获取表单数据
-                const user = this.user
-                // 2. 表单验证
-                // 3. 提交表单请求登录
-                // 4. 根据请求响应结果处理后续操作
+      // 在组件中必须通过 this.$toast 来调用 Toast组件
+      this.$toast.loading({
+        message: "登录中...",
+        forbidClick: true, // 禁用背景点击
+        duration: 0, // 持续时间，默认是 2000，如果为 0 则持续展示
+      });
+      // 3. 提交表单请求登录
+      try {
+        const res = await login(user);
+        this.$toast.success("登录成功");
+      } catch (err) {
+        if (err.response.status === 400) {
+          this.$toast.fail("手机号或验证码错误");
+        } else {
+          this.$toast.fail("登录失败, 请稍后重试");
+        }
+      }
+      // 4. 根据请求响应结果处理后续操作
+    },
+    async onSendSms() {
+        // 1. 校验手机号
+        try {
+            await this.$refs.loginForm.validate('mobile')
+            console.log('验证通过');
+        } catch(err) {
+            return console.log('验证失败', err);
+        }
+        // 2，验证通过，显示倒计时
+        this.isCountDownShow = true
+
+        // 3. 请求发送验证码
+        try {
+            const res = await sendSms(this.user.mobile)
+             this.$toast('发送成功')
+        } catch(err) {
+            // 发送失败，关闭倒计时
+            this.isCountDownShow = false
+            if(err.response.status === 429) {
+                this.$toast('发送太频繁了，请稍后重试！')
+            }else {
+                this.$toast('发送失败，请稍后重试！')
             }
         }
 
-}
+    }
+  },
+};
 </script>
 <style lang="less" scoped>
-    .login-container {
-        .toutiao {
-            font-size: 37px;
-        }
+.login-container {
+  .toutiao {
+    font-size: 37px;
+  }
 
-        .send-sms-btn {
-            width: 156px;
-            height: 46px;
-            line-height: 46px;
-            background-color: #ededed;
-            font-size: 22px;
-            color: #666;
-        }
+  .send-sms-btn {
+    width: 156px;
+    height: 46px;
+    line-height: 46px;
+    background-color: #ededed;
+    font-size: 22px;
+    color: #666;
+  }
 
-        .login-btn-wrap {
-            padding: 53px 33px;
-            .login-btn {
-                background-color: #6db4fb;
-                border: none;
-            }
-        }
-        
+  .login-btn-wrap {
+    padding: 53px 33px;
+    .login-btn {
+      background-color: #6db4fb;
+      border: none;
     }
+  }
+}
 </style>
