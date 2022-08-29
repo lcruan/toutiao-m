d5cdcd2fd33d42cb998bdc49d1e0760cffc3edae
lcruan <1416802331@qq.com>
2022-08-29 21:38:21 +0800

    文章收藏视图处理

---
+++ src/components/collect-article/index.vue
@@ -0,0 +1,31 @@
+<template>
+    <van-button
+        :icon=" value ? 'star' : 'star-o'"
+        :class="{
+            collected: value
+        }"
+      />
+</template>
+
+<script>
+    export default {
+        name: 'CollectArticle',
+        props: {
+            value: {
+                type: Boolean,
+                required: true
+            }
+        }
+    }
+</script>
+
+<style lang="less" scoped>
+    .van-button--default {
+        border-color: #fff;
+    }
+    .collected {
+        .van-icon {
+            color: #ffa500;
+        }
+    }
+</style>
